00a01fd35a9607ba701cefd5e96c29c556943afa
Nick Reisenauer <38516489+NickReisenauer@users.noreply.github.com>
2021-02-09 15:55:43 -0700

    Up to challenge on useReducer

--- react-andrew-mead/src/index.js
+++ react-andrew-mead/src/index.js
@@ -1,103 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
+
+const notesReducer = (state, action) => {
+  switch (action.type) {
+    case "POPULATE_NOTES":
+      return action.notes;
+    default:
+      return state;
+  }
+};
 
 const NoteApp = () => {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
+  const addNote = (e) => {
+    e.preventDefault();
+    // setNotes([...notes, { title, body }]);
+    setTitle("");
+    setBody("");
+  };
+
+  const removeNote = (title) => {
+    // setNotes(notes.filter((note) => note.title !== title));
+  };
+
   useEffect(() => {
-    const notesData = JSON.parse(localStorage.getItem("notes"));
-    if (notesData) setNotes(notesData);
+    const note = JSON.parse(localStorage.getItem("notes"));
+
+    if (notes) {
+      // setNotes(notesData);
+      dispatch({ type: "POPULATE_NOTES", notes });
+    }
   }, []);
 
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
+
   return (
     <div>
       <h1>Notes</h1>
-      {notes.map((note, idx) => (
-        <div key={idx}>
-          <h3>{note.title}</h3>
-          <p>{note.body}</p>
-          <button
-            onClick={(e) => {
-              setNotes(
-                notes.filter(
-                  (individualNote) => individualNote.title !== note.title
-                )
-              );
-            }}
-          >
-            Remove
-          </button>
-        </div>
+      {notes.map((note) => (
+        <Note key={note.title} note={note} removeNote={removeNote} />
       ))}
-      <p>Add a note</p>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setNotes([...notes, { title, body }]);
-          setTitle("");
-          setBody("");
-        }}
-      >
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
         <textarea
-          type="text"
           value={body}
           onChange={(e) => setBody(e.target.value)}
-        />
-        <button>Add note</button>
+        ></textarea>
+        <button>add note</button>
       </form>
     </div>
   );
 };
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count);
-  const [text, setText] = useState("");
-
+const Note = ({ note, removeNote }) => {
   useEffect(() => {
-    console.log("This should only run once");
+    console.log("Setting up effect!");
+
+    return () => {
+      console.log("Cleaning up effect!");
+    };
   }, []);
-
-  useEffect(() => {
-    console.log("useEffect ran");
-    document.title = count;
-  }, [count]);
 
   return (
     <div>
-      <p>
-        The current {text || "count"} is {count}
-      </p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+      <h3>{note.title}</h3>
+      <p>{note.body}</p>
+      <button onClick={() => removeNote(note.title)}>x</button>
     </div>
   );
 };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <NoteApp />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<NoteApp />, document.getElementById("root"));
