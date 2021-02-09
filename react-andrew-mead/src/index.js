649c23096430061e1ea748fd7fd71a4738b27194
Nick Reisenauer <38516489+NickReisenauer@users.noreply.github.com>
2021-02-09 13:41:58 -0700

    Set up to use multiple useState calls

--- react-andrew-mead/src/index.js
+++ react-andrew-mead/src/index.js
@@ -2,26 +2,79 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count);
-
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
   return (
     <div>
-      <p>The current count is {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <h1>Notes</h1>
+      {notes.map((note, idx) => (
+        <div key={idx}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button
+            onClick={(e) => {
+              setNotes(
+                notes.filter(
+                  (individualNote) => individualNote.title !== note.title
+                )
+              );
+            }}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+      <p>Add a note</p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setNotes([...notes, { title, body }]);
+          setTitle("");
+          setBody("");
+        }}
+      >
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          type="text"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <button>Add note</button>
+      </form>
     </div>
   );
 };
 
-App.defaultProps = {
-  count: 0,
-};
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count);
+//   const [text, setText] = useState("");
+
+//   return (
+//     <div>
+//       <p>
+//         The current {text || "count"} is {count}
+//       </p>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//       <button onClick={() => setCount(count - 1)}>Decrease</button>
+//       <button onClick={() => setCount(0)}>Reset</button>
+//       <input
+//         type="text"
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//       />
+//     </div>
+//   );
+// };
 
 ReactDOM.render(
   <React.StrictMode>
-    <App initialCount={99} />
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
